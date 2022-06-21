3af5e53076966000951e5351be3b3ee0a8e48923
ritikadhawan <ritikadhawan28@gmail.com>
2022-06-21 10:45:39 +0530

    Initial setup

---
+++ src/main/java/com/tw/DbMigrations/DbMigrationsApplication.java
@@ -0,0 +1,13 @@
+package com.tw.DbMigrations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbMigrationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbMigrationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tw/DbMigrations/DbMigrationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tw.DbMigrations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbMigrationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
